8f746534bcef4a436a62bcff3791ad037fb9d93d
TheodoroFelipe <fetheo.26@gmail.com>
2022-08-09 19:06:46 -0300

    cliente service

--- src/components/clientes/cadastro/index.tsx
+++ src/components/clientes/cadastro/index.tsx
@@ -2,13 +2,20 @@ import React, {useState} from "react";
 import {Layout} from "../../layout";
 import {ClienteForm} from './form'
 import {Cliente} from "../../../app/models/clientes";
+import {useClienteService} from "../../../app/services/cliente.service";
 
 export const CadastroCliente: React.FC = () => {
 
     const [cliente, setCliente] = useState<Cliente>({});
 
+    const service = useClienteService();
+
     const handleSubmit = (cliente: Cliente) => {
-        console.log(cliente);
+
+        service.salvar(cliente).then(clienteResposta => {
+            setCliente(clienteResposta.data)
+            console.log(clienteResposta.data);
+        })
     }
 
     return (
