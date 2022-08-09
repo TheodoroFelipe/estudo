3f6b50cbd57e89a979be8ad3d4e6d7a75f515e74
TheodoroFelipe <fetheo.26@gmail.com>
2022-08-09 19:06:59 -0300

    cliente service

---
+++ src/app/services/cliente.service.ts
@@ -0,0 +1,37 @@
+import {httpClient} from "../http/indexhttp";
+import {Cliente} from "../models/clientes";
+import {AxiosResponse} from "axios";
+
+const resourceURL: string = "/api/clientes"
+
+export const useClienteService =() => {
+
+    const salvar = async (cliente: Cliente): Promise<AxiosResponse<Cliente>> => {
+        //const response: AxiosResponse<Cliente> = await httpClient.post<Cliente>(resourceURL, cliente);
+        return httpClient.post<Cliente>(resourceURL, cliente);
+    }
+
+    const atualizar = async (cliente: Cliente): Promise<void> => {
+        const url: string = `${resourceURL}/${cliente.id}`
+        await httpClient.put<Cliente>(url, cliente)
+    }
+
+    const carregarCliente = async (id) : Promise<Cliente> => {
+        const url: string = `${resourceURL}/${id}`
+        const response: AxiosResponse<Cliente> = await httpClient.get(url);
+        return response.data;
+    }
+
+    const deletar = async (id) : Promise<void> => {
+        const url: string = `${resourceURL}/${id}`
+        await httpClient.delete(url)
+    }
+
+    return{
+        salvar,
+        atualizar,
+        carregarCliente,
+        deletar
+    }
+
+ }
